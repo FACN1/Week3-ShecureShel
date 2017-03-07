01a4dbc585b1242ff0e442e10602c35b50138c9e
Edgar Dewsbery <edewsbery@gmail.com>
2017-03-07 19:16:26 +0200

    retrieve input and remove formatting

--- javascript/input.js
+++ javascript/input.js
@@ -1,1 +1,36 @@
-console.log("input linked");
+
+
+var searchForm = document.getElementById('user-search');
+searchForm.addEventListener('submit', function(event){
+  event.preventDefault();
+  var ingredientsInput = event.target.childNodes[1].value;
+
+  return formatInput(ingredientsInput);
+});
+
+function formatInput(input){
+  var searchWord = input.slice();
+  searchWord = searchWord.trim().toLowerCase();
+  searchWord = searchWord.replace(/\s/g, ',');
+
+  console.log(searchWord);
+
+}
+
+
+;// .toLowerCase, remove special char, remove whitespace from top and tail, add commas,
+
+//prefix with i=
+
+// return "i=onions,garlic";
+// formatInput("    Onions, garlic + brocolli  ")
+
+
+/*
+
+1. retrieve input string from the DOM
+2. format searchWord
+
+
+3. add error handler function
+*/
