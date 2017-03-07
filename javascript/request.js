6074a9306db2ae8a41f622a0271ae9ffa52bd69e
suha-edris <suha.edris@gmail.com>
2017-03-07 18:44:40 +0200

    make request.js

--- javascript/request.js
+++ javascript/request.js
@@ -1,1 +1,17 @@
-console.log("request linked");
+var base = "http://www.recipepuppy.com/api/?";
+var xhr = new XMLHttpRequest();
+var searchWord="i=onions"; // needs to be updated to feed from input.js
+var url = base+searchWord;
+xhr.onreadystatechange = function ()
+{
+  if(xhr.readyState == 4 && xhr.status == 200)
+  {
+    var gitObj= JSON.parse(xhr.responseText);
+    console.log(gitObj);
+
+
+
+  }
+};
+xhr.open("GET", url, true);
+xhr.send();
