c156044ef3062c068883b8ef478fe531cca83312
Lawson <lgbeaty@live.ca>
2017-03-08 17:16:26 +0200

    added simple test for input formatting

--- tests/tests.js
+++ tests/tests.js
@@ -1,3 +1,5 @@
-QUnit.test( "hello test", function( assert ) {
-  assert.ok( 1 == "1", "Passed!" );
+//Test that formatInput function is working properly
+QUnit.test("Test that formatInput is operating correctly", function( assert ) {
+  var inputTestOne = "   eggs cHicken Bacon";
+  assert.deepEqual(formatInput(inputTestOne), "i=eggs,chicken,bacon", "FormatInput function has correctly formatted the input");
 });
