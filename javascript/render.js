6361b9bf7bb1c040482e87f8962de717117f0fb4
suha-edris <suha.edris@gmail.com>
2017-03-08 14:54:18 +0200

    render.js and request.js updated

--- javascript/render.js
+++ javascript/render.js
@@ -1,1 +1,26 @@
-console.log("render linked");
+// console.log("render linked");
+function renderSearch (recipeObject)
+{
+  var recipeArray=recipeObject.results;
+  var titleList =document.getElementById("results-list")
+  recipeArray.forEach(function (element)
+  {
+
+      var listNode = document.createElement('li');
+      var itemNode = document.createElement('a');
+
+      var title= element.title;
+      itemNode.href= element.href;
+      itemNode.textContent=title;
+
+      titleList.appendChild(listNode);
+      listNode.appendChild(itemNode);
+
+  });
+
+  return
+   {
+    recipeArray:recipeArray
+   }
+
+}
