5b0a3ff544dff986ed72fc1f696417b14f68b6eb
Edgar Dewsbery <edewsbery@gmail.com>
2017-03-08 11:49:11 +0200

    modularise input.js

--- javascript/input.js
+++ javascript/input.js
@@ -1,21 +1,28 @@
 /* render ingredients input */
 
 var searchForm = document.getElementById('user-search');
+
 searchForm.addEventListener('submit', function(event){
   event.preventDefault();
-  var prefix = "i=";
   var ingredientsInput = event.target.childNodes[1].value.slice();
   if (ingredientsInput === "") {
     alert("Write an ingredient to find a recipe");
     return;
   }
-  ingredientsInput = ingredientsInput.trim().toLowerCase();
-  ingredientsInput = ingredientsInput.replace(/\s/g, ',');
-  // return formatInput(ingredientsInput);
-  var ingredientsSearch = prefix+ingredientsInput;
-  console.log(ingredientsSearch);
-  return ingredientsSearch;
+
+  formatInput(ingredientsInput);
 });
 
+function formatInput(input){
+  var inputText = input.slice();
+
+  var prefix = "i="; // this should be its own function.
+
+  inputText = inputText.trim().toLowerCase();
+  inputText = inputText.replace(/\s/g, ',');
+  var ingredientsSearch = prefix+inputText;
+  makeRequest(ingredientsSearch);
+
+}
 
 /* end render ingredients input */
