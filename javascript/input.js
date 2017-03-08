70c6f7e05e1677bbb604de155b7bd14ecdc3a1af
Edgar Dewsbery <edewsbery@gmail.com>
2017-03-08 10:39:14 +0200

    render user input and return search word for XHR request

--- javascript/input.js
+++ javascript/input.js
@@ -1,36 +1,21 @@
-
+/* render ingredients input */
 
 var searchForm = document.getElementById('user-search');
 searchForm.addEventListener('submit', function(event){
   event.preventDefault();
-  var ingredientsInput = event.target.childNodes[1].value;
-
-  return formatInput(ingredientsInput);
+  var prefix = "i=";
+  var ingredientsInput = event.target.childNodes[1].value.slice();
+  if (ingredientsInput === "") {
+    alert("Write an ingredient to find a recipe");
+    return;
+  }
+  ingredientsInput = ingredientsInput.trim().toLowerCase();
+  ingredientsInput = ingredientsInput.replace(/\s/g, ',');
+  // return formatInput(ingredientsInput);
+  var ingredientsSearch = prefix+ingredientsInput;
+  console.log(ingredientsSearch);
+  return ingredientsSearch;
 });
 
-function formatInput(input){
-  var searchWord = input.slice();
-  searchWord = searchWord.trim().toLowerCase();
-  searchWord = searchWord.replace(/\s/g, ',');
 
-  console.log(searchWord);
-
-}
-
-
-;// .toLowerCase, remove special char, remove whitespace from top and tail, add commas,
-
-//prefix with i=
-
-// return "i=onions,garlic";
-// formatInput("    Onions, garlic + brocolli  ")
-
-
-/*
-
-1. retrieve input string from the DOM
-2. format searchWord
-
-
-3. add error handler function
-*/
+/* end render ingredients input */
