5902ae217c3593e7f9f16e9ef5e19fcc825da934
Edgar Dewsbery <edewsbery@gmail.com>
2017-03-09 13:00:21 +0200

    #35 modularise javascript files

--- javascript/render.js
+++ javascript/render.js
@@ -1,39 +1,38 @@
-function renderSearch (recipeObject) {
+function renderSearch(recipeObject) {
+    var recipeArray = recipeObject.results;
+    var titleList = document.getElementById("results-list");
+    titleList.innerHTML = "";
+    var headerTitle = document.createElement('header');
+    headerTitle.textContent = "Recipes";
+    headerTitle.className = "headerTitleCSS";
+    titleList.appendChild(headerTitle);
 
-  var recipeArray=recipeObject.results;
-  var titleList =document.getElementById("results-list");
-  titleList.innerHTML="";
-  var headerTitle = document.createElement('header');
-  headerTitle.textContent="Recipes";
-  headerTitle.className="headerTitleCSS";
-  titleList.appendChild(headerTitle);
+    recipeArray.forEach(function(element) {
 
-  recipeArray.forEach(function (element){
+        var listNode = document.createElement('li');
+        var itemNode = document.createElement('a');
 
-      var listNode = document.createElement('li');
-      var itemNode = document.createElement('a');
+        var title = element.title;
+        itemNode.href = element.href;
+        itemNode.target = "_blank";
+        itemNode.textContent = title;
 
-      var title= element.title;
-      itemNode.href= element.href;
-      itemNode.target="_blank";
-      itemNode.textContent=title;
-
-      titleList.appendChild(listNode);
-      listNode.appendChild(itemNode);
-
-
-      var listNodeIngredients = document.createElement('li');
-
-      var ingredients=element.ingredients;
-      listNodeIngredients.textContent=ingredients;
-
-      titleList.appendChild(listNodeIngredients);
+        titleList.appendChild(listNode);
+        listNode.appendChild(itemNode);
 
 
-  });
+        var listNodeIngredients = document.createElement('li');
 
-  return{
-    recipeArray:recipeArray
-   }
+        var ingredients = element.ingredients;
+        listNodeIngredients.textContent = ingredients;
 
-}
+        titleList.appendChild(listNodeIngredients);
+
+
+    });
+
+    return {
+        recipeArray: recipeArray
+    }
+
+};
